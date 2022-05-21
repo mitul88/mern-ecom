369379bb580bcef3ad170cb5d4cf8a0798b79577
shahed <shahed.rahman88@gmail.com>
2022-05-21 18:07:50 +0600

    project setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(process.env.MONGODB_URL_LOCAL)
+
+mongoose.connect(process.env.MONGODB_URL_LOCAL)
+    .then(()=> console.log("Mongodb connected"))
+    .catch(err=> console.error("Mongo connection error, check your mongo connectivity", err))
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, ()=> {
+    console.log(`App running on ${port}` )
+})
