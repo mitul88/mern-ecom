6c9419803c1bab7731b7f409bdd8308d5706b52d
shahed <shahed.rahman88@gmail.com>
2022-05-24 20:40:18 +0600

    category create func added

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,8 +2,20 @@ const _ = require('lodash');
 const {Category, validate} = require('../models/category');
 
 module.exports.createCategory = async (req, res) => {
-    
+    const {error} = validate(_.pick(req.body, ["name"]));
+
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const category = new Category(_.pick(req.body, ["name"]));
+    const result = await category.save();
+    return res.status(201).send({
+        message: "Category created successfully!",
+        data: {
+            name: result.name
+        }
+    })
 }
+
 module.exports.getCategories = async (req, res) => {
 
 }
