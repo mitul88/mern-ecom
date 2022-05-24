1c5a3507fda4da87bb9eb7ae6c3976351d4b2bbe
shahed <shahed.rahman88@gmail.com>
2022-05-24 20:10:48 +0600

    category model created

--- models/category.js
+++ models/category.js
@@ -1,5 +1,6 @@
 const {Schema, model} = require('mongoose');
 const Joi = require('joi');
+const { join } = require('lodash');
 
 module.exports.Category = model('Category', Schema({
     name: {
@@ -7,3 +8,10 @@ module.exports.Category = model('Category', Schema({
         unique: true
     }
 }, { timestamps: true }));
+
+module.exports.validate = category => {
+    const schema = Joi.object({
+        name: join.string().min(3).max(50).required()
+    });
+    return schema.validate(category);
+}
