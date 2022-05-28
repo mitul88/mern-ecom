dd85a3fa5bf790f5374d4456be51227710777795
shahed <shahed.rahman88@gmail.com>
2022-05-28 15:04:23 +0600

    get single product and photo

--- controllers/productController.js
+++ controllers/productController.js
@@ -48,7 +48,21 @@ module.exports.getProducts = async (req, res) => {
 }
 
 module.exports.getProductById = async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId)
+                        .select({ photo: 0 })
+                        .populate('category', name);
+    
+    if(!product) return res.status(404).send("Product not found!!");
+    return res.status(200).send(product);
+}
 
+module.exports.getPhoto = async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product(productId)
+            .select({ photo: 1, _id: 0 })
+    res.set('Content-Type', product.photo.contentType);
+    return res.status(200).send((product.photo.data));
 }
 
 module.exports.updateProductById = async (req, res) => {
