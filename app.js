7b5b29d3ea6b7ced75590554d1c25870e39f8346
shahed <shahed.rahman88@gmail.com>
2022-05-28 22:51:35 +0600

    relocate all routes to routes middleware

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const express = require('express');
 require('express-async-errors');
 const app = express();
-
-const userRouter = require('./routers/userRouter');
-const categoryRouter = require('./routers/categoryRouter');
-const productRouter = require('./routers/productRouter');
-
 const error = require('./middlewares/error');
 
 require('./middlewares')(app);
-
-app.use('/api/user', userRouter);
-app.use('/api/category', categoryRouter);
-app.use('/api/product', productRouter);
-
+require('./middlewares/routes')(app);
 
 // The express-aync-error needs to placed after router i.e. userRouter middleware. since userRouter does'nt have any next func
 app.use(error)
+
 
 module.exports= app;
