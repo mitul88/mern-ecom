9ef8bae5509fc4052d226167b585e63f15f6b8d1
shahed <shahed.rahman88@gmail.com>
2022-05-28 20:08:18 +0600

    filterProduct initiated

--- controllers/productController.js
+++ controllers/productController.js
@@ -101,5 +101,16 @@ module.exports.updateProductById = async (req, res) => {
 }
 
 module.exports.filterProducts = async (req, res) => {
+    let order = req.body.order === 'desc'? -1 : 1;
+    let sortBy = req.body.sortBy ? req.body.sortBy : 'createdAt';
+    let limit = req.body.limit ? parseInt(req.body.limit) : 10;
+    let skip = parseInt(req.body.skip);
 
+    const products = await Product.find()
+        .select({ photo: 0 })
+        .populate()
+        .sort({ [sortBy] : order })
+        .skip(skip)
+        .limit(limit)
+    return res.status(200).send(products);
 }
