f7b4dabea0538912a19cce4fec9cabb184fd7d27
shahed <shahed.rahman88@gmail.com>
2022-05-28 20:56:11 +0600

    filter args passed

--- controllers/productController.js
+++ controllers/productController.js
@@ -105,8 +105,27 @@ module.exports.filterProducts = async (req, res) => {
     let sortBy = req.body.sortBy ? req.body.sortBy : 'createdAt';
     let limit = req.body.limit ? parseInt(req.body.limit) : 10;
     let skip = parseInt(req.body.skip);
+    let filters = req.body.filters;
 
-    const products = await Product.find()
+    let args = {};
+
+    for(let key in filters) {
+        if(filters[key].length > 0 ){
+            if(key === "price") {
+                args["price"] = {
+                    $gte : filters["price"][0],
+                    $lte : filters["price"][1]
+                }
+            }
+            if(key === "category") {
+                args["category"] = {
+                    $in : filters["category"]
+                }
+            }
+        }
+    }
+
+    const products = await Product.find(args)
         .select({ photo: 0 })
         .populate()
         .sort({ [sortBy] : order })
